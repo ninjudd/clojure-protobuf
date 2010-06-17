7d735cfabd0e739017300241335cb5cce41eb92c
Justin Balthrop <git@justinbalthrop.com>
2010-06-16 17:11:59 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-protobuf "0.2.0"
+(defproject clojure-protobuf "0.2.1-SNAPSHOT"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure         "1.2.0-master-SNAPSHOT"]
                  [clojure-contrib "1.2.0-SNAPSHOT"]]
