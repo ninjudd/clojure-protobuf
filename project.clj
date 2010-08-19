03bf2a8bd133dd29fce5357cbce366154a4620c0
Justin Balthrop <git@justinbalthrop.com>
2010-08-19 08:42:12 -0700

    update to clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clojure-protobuf "0.2.7"
+(defproject clojure-protobuf "0.2.8"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
-  :dependencies [[clojure         "1.2.0-master-SNAPSHOT"]
-                 [clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[clojure "1.2.0"]]
   :tasks [protobuf.tasks]
   :jar-files ["proto"])
