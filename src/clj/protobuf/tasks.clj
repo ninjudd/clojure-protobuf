89443431d0479d53f77f4adb0f4a6b539bbca322
lance bradley <l@ncebradley.org>
2010-11-17 15:34:55 -0800

    cake face compatible branch

--- src/clj/protobuf/tasks.clj
+++ src/clj/protobuf/tasks.clj
@@ -2,7 +2,7 @@
   (:use cake cake.core cake.ant
         [cake.file :only [file]]
         [cake.project :only [log]]
-        [cake.tasks.deps :only [os-name]]
+        [cake.utils :only [os-name prompt-read]]
         [cake.tasks.compile :only [compile-java]]
         [clojure.java.shell :only [sh]]
         [clojure.java.io :only [reader]]
