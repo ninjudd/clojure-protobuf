983b8393a6cb62e97195a608bfd1a3c3eab614a0
ninjudd <git@justinbalthrop.com>
2011-03-18 12:19:50 -0700

    version 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-protobuf "0.3.4-SNAPSHOT"
+(defproject clojure-protobuf "0.3.4"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]]
   :tasks [protobuf.tasks]
