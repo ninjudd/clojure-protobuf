7ea3d1a8e6dba1edc63ef856a7befe908fe99c68
Justin Balthrop <git@justinbalthrop.com>
2011-06-29 18:21:23 -0700

    updates for cake depot

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clojure-protobuf "0.4.6"
+(defproject clojure-protobuf "0.4.7-SNAPSHOT"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.2.2"]]
   :tasks [protobuf.tasks]
+  :source-path ["src/clj" "src/jvm"]
   :jar-files ["proto"])
