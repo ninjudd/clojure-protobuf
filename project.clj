80780fe01039284125527f5739f5410e339772ac
Justin Balthrop <git@justinbalthrop.com>
2011-08-18 17:40:33 -0700

    bump cake-marginalia

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.2.2"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.3"]]
   :tasks [protobuf.tasks cake-marginalia.tasks]
   :source-path ["src/clj" "src/jvm"]
   :jar-files ["proto"])
