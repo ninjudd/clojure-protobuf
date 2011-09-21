d4b6f0d9ba2196b4a84ee0b403ff7b1b83457510
Justin Balthrop <git@justinbalthrop.com>
2011-09-20 19:40:30 -0700

    alpha4

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject protobuf "0.5.0-alpha3"
+(defproject protobuf "0.5.0-alpha4"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.2.2"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.3"]]
-  :tasks [protobuf.tasks cake-marginalia.tasks]
+;; :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.3"]]
+  :cake-plugins [[cake-protobuf "0.5.0-alpha4"]]
+;; :tasks [cake-marginalia.tasks]
   :source-path ["src/clj" "src/jvm"]
   :jar-files ["proto"])
