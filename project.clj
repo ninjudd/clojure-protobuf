eda395d3eb223bc6761bb360a5f291cf672c0467
Anthony Grimes <i@raynes.me>
2011-12-09 16:23:11 -0600

    io version inc.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject protobuf "0.5.0-beta4"
+(defproject protobuf "0.5.0-beta5"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.2.2"]
                  [useful "0.7.4-alpha4"]
                  [classlojure "0.6.3"]]
   :dev-dependencies [[gloss "0.2.0-rc1"]
-                     [io "0.1.0-alpha1"]]
+                     [io "0.1.0-alpha2"]]
   :eval-in-project true
   :java-source-path "build/protosrc"
   :jar-files ["proto"])
