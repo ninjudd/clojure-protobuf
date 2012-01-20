dd55f46dec76225c68a9d203699859b673b83447
Justin Balthrop <git@justinbalthrop.com>
2012-01-19 19:36:23 -0800

    0.6.0-beta4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protobuf "0.6.0-beta3"
+(defproject protobuf "0.6.0-beta4"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :dependencies [[clojure "1.2.0"]
                  [ordered-set "0.2.3"]
