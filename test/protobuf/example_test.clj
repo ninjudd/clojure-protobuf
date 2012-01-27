311791dbc30096994db0e2de131ab91c8132fa93
Anthony Grimes <i@raynes.me>
2012-01-27 16:46:13 -0600

    Update example-test with expected output.
    Also cleaned up the test significantly.

--- test/protobuf/example_test.clj
+++ test/protobuf/example_test.clj
@@ -3,8 +3,9 @@
 
 (def Photo (protodef protobuf.test.Example$Photo))
 
-;; Haven't seen correct output so I have nothing to compare to.
+(def data {:id 7, :path "/photos/h2k3j4h9h23", :labels #{"hawaii" "family" "surfing"}, 
+           :attrs {"color space" "RGB", "dimensions" "1632x1224", "alpha" "no"}, 
+           :tags {4 {:person-id 4, :x-coord 607, :y-coord 813, :width 25, :height 27}}})
+
 (deftest example-test
-  (is (protobuf Photo :id 7  :path "/photos/h2k3j4h9h23" :labels #{"hawaii" "family" "surfing"}
-                      :attrs {"dimensions" "1632x1224", "alpha" "no", "color space" "RGB"}
-                      :tags  {4 {:person_id 4, :x_coord 607, :y_coord 813, :width 25, :height 27}})))
+  (is (= data (apply protobuf Photo (apply concat data)))))
