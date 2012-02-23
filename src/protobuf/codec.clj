99d77e123c17e99ddd45a28d125d9aba1f9ba40c
Alan Malloy <alan@malloys.org>
2012-02-22 19:12:34 -0800

    Fix handling of reset/length fields

--- src/protobuf/codec.clj
+++ src/protobuf/codec.clj
@@ -9,22 +9,24 @@
 
 (declare protobuf-codec)
 
-(let [len-key :proto_length]
-  (defn length-prefix [proto]
-    (let [proto (protodef proto)
-          min   (alength (protobuf-dump proto {len-key 0}))
-          max   (alength (protobuf-dump proto {len-key Integer/MAX_VALUE}))]
-      (letfn [(check [test msg]
-                (when-not test
-                  (throw (Exception. (format "In %s: %s %s"
-                                             (.getFullName proto) (name len-key) msg)))))]
-        (check (pos? min)
-               "field is required for repeated protobufs")
-        (check (= min max)
-               "must be of type fixed32 or fixed64"))
-      (gloss/compile-frame (gloss/finite-frame max (protobuf-codec proto))
-                           #(hash-map len-key %)
-                           len-key))))
+(def ^{:private true} len-key :proto_length)
+(def ^{:private true} reset-key :codec_reset)
+
+(defn length-prefix [proto]
+  (let [proto (protodef proto)
+        min   (alength (protobuf-dump proto {len-key 0}))
+        max   (alength (protobuf-dump proto {len-key Integer/MAX_VALUE}))]
+    (letfn [(check [test msg]
+              (when-not test
+                (throw (Exception. (format "In %s: %s %s"
+                                           (.getFullName proto) (name len-key) msg)))))]
+      (check (pos? min)
+             "field is required for repeated protobufs")
+      (check (= min max)
+             "must be of type fixed32 or fixed64"))
+    (gloss/compile-frame (gloss/finite-frame max (protobuf-codec proto))
+                         #(hash-map len-key %)
+                         len-key)))
 
 (defn protobuf-codec [proto & {:keys [validator repeated]}]
   (let [proto (protodef proto)]
@@ -43,5 +45,13 @@
                 val
                 (protobuf proto val))))))
         (fix repeated
-             #(gloss/repeated (gloss/finite-frame (length-prefix proto) %) :prefix :none))
+             #(gloss/compile-frame (gloss/repeated (gloss/finite-frame (length-prefix proto) %)
+                                                   :prefix :none)
+                                   identity
+                                   (partial dissoc len-key)))
+        (gloss/compile-frame identity
+                             (fn [data]
+                               (-> data
+                                   (dissoc reset-key :revisions)
+                                   (with-meta (select-keys data [:revisions])))))
         (with-meta {:schema (protobuf-schema proto)}))))
