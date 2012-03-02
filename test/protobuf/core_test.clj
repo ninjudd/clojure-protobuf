c2f2ed37d6df3188c1b1da5ef70ab96efcb8d4bd
Alan Malloy <alan@malloys.org>
2012-03-01 17:10:02 -0800

    Update test for changes to protobuf.
    If you assoc nil into a protobuf with a nullable field, the key should
    be present but with a value of nil.

--- test/protobuf/core_test.clj
+++ test/protobuf/core_test.clj
@@ -182,20 +182,21 @@
              (get-raw p :time))))))
 
 (deftest test-nullable
-  (let [p (protobuf Bar :int 1 :long 330000000000 :flt 1.23 :dbl 9.87654321 :str "foo")]
+  (let [p (protobuf Bar :int 1 :long 330000000000 :flt 1.23 :dbl 9.87654321 :str "foo")
+        keyset #{:int :long :flt :dbl :str}]
     (is (= 1            (get p :int)))
     (is (= 330000000000 (get p :long)))
     (is (= (float 1.23) (get p :flt)))
     (is (= 9.87654321   (get p :dbl)))
     (is (= "foo"        (get p :str)))
-    (is (= [:int :long :flt :dbl :str] (keys p)))
+    (is (= keyset (set (keys p))))
     (let [p (append p {:int nil :long nil :flt nil :dbl nil :str nil})]
       (is (= nil (get p :int)))
       (is (= nil (get p :long)))
       (is (= nil (get p :flt)))
       (is (= nil (get p :dbl)))
       (is (= nil (get p :str)))
-      (is (= nil (keys p))))
+      (is (= keyset (set (keys p)))))
     (testing "nullable successions"
       (let [p (protobuf Bar :label "foo")]
         (is (= "foo" (get p :label)))
