d5beef3dec3a7f7df007dc6dd604050ac7ca87c6
Anthony Grimes <i@raynes.me>
2012-04-20 12:31:49 -0700

    Updated gloss to remove contrib dependency.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject protobuf "0.6.0-beta15"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
-  :dependencies [[clojure "1.2.0"]
+  :dependencies [[clojure "1.4.0"]
                  [ordered-set "0.2.3"]
                  [useful "0.8.0-alpha1"]
                  [schematic "0.0.5"]]
-  :dev-dependencies [[gloss "0.2.0-rc1"]
+  :dev-dependencies [[gloss "0.2.1-beta1"]
                      [io "0.1.0-alpha2"]
                      [lein-protobuf "0.6.0-beta15"]]
   :hooks [leiningen.protobuf]
