4d94be836b3bf41a1808c362b8212990539b2feb
Anthony Grimes <i@raynes.me>
2012-06-29 18:26:16 -0500

    Move leiningen plugin to a subproject.

---
+++ lein-protobuf/project.clj
@@ -0,0 +1,10 @@
+(defproject lein-protobuf "0.1.0-alpha1"
+  :description "Leiningen plugin for clojure-protobuf."
+  :dependencies [[fs "1.0.0"]
+                 [conch "0.2.0"]]
+  :eval-in-leiningen true
+  ;; Bug in the current 1.x branch of Leiningen causes
+  ;; jar to implicitly clean no matter what, wiping stuff.
+  ;; This prevents that.
+  :disable-implicit-clean true
+  :checksum-deps true)
--- project.clj
+++ project.clj
@@ -1,15 +1,13 @@
-(defproject protobuf "0.6.0-beta18"
+(defproject protobuf "0.6.0-beta19"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [ordered-collections "0.4.0"]
                  [useful "0.8.2-alpha1"]
-                 [fs "1.0.0"]
-                 [conch "0.2.0"]
                  [schematic "0.0.5"]]
-  :dev-dependencies [[gloss "0.2.0-rc1"]
-                     [io "0.2.0-beta2"]]
+  :plugins [[lein-protobuf "0.1.0-alpha1"]]
+  :profiles {:dev {:dependencies [[gloss "0.2.1"]
+                                  [io "0.2.0-beta2"]]}}
   :hooks [leiningen.protobuf]
-  :eval-in-leiningen true
   ;; Bug in the current 1.x branch of Leiningen causes
   ;; jar to implicitly clean no matter what, wiping stuff.
   ;; This prevents that.
