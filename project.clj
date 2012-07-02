c792f26d5eb8ae3586d8ed18276239fbb78bc942
Anthony Grimes <i@raynes.me>
2012-07-02 17:56:29 -0500

    Add :url and :license keys to the protobuf project.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject protobuf "0.6.0-beta19"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/flatland/clojure-protobuf"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ordered-collections "0.4.0"]
                  [useful "0.8.2-alpha1"]
