4f2569bada7004b8dc348eb7004454b410336259
Justin Balthrop <git@justinbalthrop.com>
2012-07-18 14:56:26 -0700

    update lein-protobuf

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protobuf "0.6.1-beta1"
+(defproject protobuf "0.6.1-beta2"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,7 +7,7 @@
                  [ordered-collections "0.4.0"]
                  [useful "0.8.2-alpha1"]
                  [schematic "0.0.5"]]
-  :plugins [[lein-protobuf "0.2.0-beta1"]]
+  :plugins [[lein-protobuf "0.2.0-beta2"]]
   :profiles {:dev {:dependencies [[gloss "0.2.1"]
                                   [io "0.2.0-beta2"]]}}
   :protobuf-version "2.4.1"
