b9f8e4eea437f5a3fedbcea278456d10e199e8da
Anthony Grimes <i@raynes.me>
2012-07-24 17:34:54 -0500

    Test against multiple Clojure versions.

--- project.clj
+++ project.clj
@@ -7,9 +7,15 @@
                  [ordered-collections "0.4.0"]
                  [useful "0.8.2-alpha1"]
                  [schematic "0.0.5"]]
-  :plugins [[lein-protobuf "0.2.0-beta2"]]
-  :profiles {:dev {:dependencies [[gloss "0.2.1"]
+  :plugins [[lein-protobuf "0.2.0-beta3"]]
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+             :dev {:dependencies [[gloss "0.2.1"]
                                   [io "0.2.0-beta2"]]}}
+  :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :protobuf-version "2.4.1"
   :hooks [leiningen.protobuf]
   :checksum-deps true
