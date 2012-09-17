9742dae754170e98fe74c61f52f34836b0b523d9
Justin Balthrop <git@justinbalthrop.com>
2012-09-17 12:10:31 -0700

    add tasks.clj

---
+++ tasks.clj
@@ -0,0 +1,14 @@
+(ns tasks
+  (:use [cake.core :only [deftask]]
+        [cake.file :only [file cp mkdir]]
+        [cake.tasks.protobuf :only [protoc srcdir]]))
+
+(defn build-protobuf []
+  (protoc ["protobuf/core/extensions.proto" "clojure/protobuf/test.proto"]))
+
+(deftask protobuf.google
+  (mkdir "proto/google/protobuf")
+  (cp (str srcdir "/src/google/protobuf/descriptor.proto") "proto/google/protobuf")
+  (protoc ["google/protobuf/descriptor.proto"] (str srcdir "/java/src/main/java")))
+
+(deftask proto #{protobuf.google})
