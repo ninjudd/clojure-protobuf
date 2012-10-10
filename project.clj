af8c6f25ed91dad90f0a5791ae0b14635610c531
Justin Balthrop <git@justinbalthrop.com>
2012-10-10 10:32:30 -0700

    compile for java 5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protobuf "0.6.2-alpha2"
+(defproject protobuf "0.6.2-SNAPSHOT"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,6 +8,7 @@
                  [useful "0.8.4"]
                  [schematic "0.0.5"]]
   :plugins [[lein-protobuf "0.2.1"]]
+  :javac-options ["-target" "5" "-source" "5"]
   :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
