1db020ffd8ee06dfb4f41b1e9e80573e319712e5
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 16:36:10 -0800

    0.7.0

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject org.flatland/protobuf "0.6.3-SNAPSHOT"
+(defproject org.flatland/protobuf "0.7.0"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/flatland/clojure-protobuf"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.8.9-SNAPSHOT"]
-                 [org.flatland/schematic "0.1.0-SNAPSHOT"]
+                 [org.flatland/useful "0.9.0"]
+                 [org.flatland/schematic "0.1.0"]
                  [ordered-collections "0.4.0"]]
-  :plugins [[lein-protobuf "0.2.3-SNAPSHOT"]]
+  :plugins [[lein-protobuf "0.3.0"]]
   :javac-options ["-target" "5" "-source" "5"]
   :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[gloss "0.2.1"]
-                                  [org.flatland/io "0.2.2-SNAPSHOT"]]}}
+                                  [org.flatland/io "0.3.0"]]}}
   :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
