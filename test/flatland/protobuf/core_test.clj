ede02a9d6716a2eeba35b169a93e5c0de6b60d50
Alan Malloy <alan@malloys.org>
2012-12-31 15:12:36 -0800

    Add a test for append/overwrite.
    The test fails due to some recent changes I don't understand.
    @ninjudd, can you have a look at this? It fails on your map-deleted
    snapshot, but works on the previous release (0.7.1).

--- test/flatland/protobuf/core_test.clj
+++ test/flatland/protobuf/core_test.clj
@@ -143,9 +143,11 @@
 
 (deftest test-append-bytes
   (let [p (protobuf Foo :id 5 :label "rad" :deleted true
-                    :tags ["sweet"] :tag-set #{"foo" "bar" "baz"})
+                    :tags ["sweet"] :tag-set #{"foo" "bar" "baz"}
+                    :deletions {"first" {:deleted false} "second" {:deleted false}})
         q (protobuf Foo :id 43 :deleted false
-                    :tags ["savory"] :tag-set {"bar" false "foo" false "bap" true})
+                    :tags ["savory"] :tag-set {"bar" false "foo" false "bap" true}
+                    :deletions {"first" {:deleted true}})
         r (protobuf Foo :label "bad")
         s (protobuf-load Foo (catbytes (protobuf-dump p) (protobuf-dump q)))
         t (protobuf-load Foo (catbytes (protobuf-dump p) (protobuf-dump r)))]
@@ -157,6 +159,8 @@
     (is (= ["sweet" "savory"] (s :tags)))
     (is (= #{"foo" "bar" "baz"} (p :tag-set)))
     (is (= #{"bap" "baz"} (s :tag-set)))
+    (is (= (s :deletions) {"first" {:id "first" :deleted true}
+                           "second" {:id "second" :deleted false}}))
     (is (= false (r :deleted)))))
 
 (deftest test-manual-append
@@ -395,7 +399,12 @@
                              :keys   {:type :string}
                              :values {:type :struct, :name "flatland.protobuf.test.core.Item"
                                       :fields {:item   {:type :string},
-                                               :exists {:default true, :type :boolean}}}}}}]
+                                               :exists {:default true, :type :boolean}}}}
+                  :deletions {:type :map
+                              :keys {:type :string}
+                              :values {:type :struct, :name "flatland.protobuf.test.core.Deletable"
+                                       :fields {:id {:type :string}
+                                                :deleted {:type :boolean}}}}}}]
     (is (= fields (protobuf-schema Foo)))
     (is (= fields (protobuf-schema flatland.protobuf.test.Core$Foo)))))
 
@@ -426,7 +435,7 @@
     (is (= [:yes :not_sure :maybe :not_sure :no] (:responses underscores)))
 
     (is (= #{:id :label :tags :parent :responses :tag_set :deleted :attr_map :foo_by_id
-             :pair_map :groups :doubles :floats :item_map :counts :time :lat :long}
+             :pair_map :groups :doubles :floats :item_map :counts :time :lat :long :deletions}
            (-> (protobuf-schema FooUnder) :fields keys set)))))
 
 (deftest test-protobuf-nested-message
