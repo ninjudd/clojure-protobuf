2ddec2ceabb590d374c9d6cd1584a6e95bf368e6
Alan Malloy <alan@malloys.org>
2013-10-30 12:42:48 -0700

    Add test for bytestrings

--- test/flatland/protobuf/example_test.clj
+++ test/flatland/protobuf/example_test.clj
@@ -1,11 +1,13 @@
 (ns flatland.protobuf.example-test
-  (:use flatland.protobuf.core clojure.test))
+  (:use flatland.protobuf.core clojure.test)
+  (:import com.google.protobuf.ByteString))
 
 (def Photo (protodef flatland.protobuf.test.Example$Photo))
 
-(def data {:id 7, :path "/photos/h2k3j4h9h23", :labels #{"hawaii" "family" "surfing"}, 
-           :attrs {"color space" "RGB", "dimensions" "1632x1224", "alpha" "no"}, 
-           :tags {4 {:person-id 4, :x-coord 607, :y-coord 813, :width 25, :height 27}}})
+(def data {:id 7, :path "/photos/h2k3j4h9h23", :labels #{"hawaii" "family" "surfing"},
+           :attrs {"color space" "RGB", "dimensions" "1632x1224", "alpha" "no"},
+           :tags {4 {:person-id 4, :x-coord 607, :y-coord 813, :width 25, :height 27}}
+           :image (ByteString/copyFrom (byte-array (map unchecked-byte [1 2 3 4 -1])))})
 
 (deftest example-test
   (is (= data (apply protobuf Photo (apply concat data)))))
