1f73a0ff14a85bea7e83f419f25442a411e1be37
Gregory Haskins <gregory.haskins@gmail.com>
2016-05-17 14:57:34 -0400

    lein-release plugin: preparing 0.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.ghaskins/protobuf "0.2-SNAPSHOT"
+(defproject org.clojars.ghaskins/protobuf "0.2"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
